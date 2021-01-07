ff9d68d5cf59a2cdb87ab59aba50603549c15b97
Richard Kazanowski <rjkaz@comcast.net>
2021-01-07 17:36:02 -0500

    update

---
+++ addclass testing/script.js
@@ -0,0 +1,19 @@
+function myFunctionRed() {
+    var element = document.getElementById("myDiv");
+    element.classList.add("red");
+}
+
+function myFunctionBlue() {
+    var element = document.getElementById("myDiv");
+    element.classList.add("blue");
+}
+
+function myFunctionPurple() {
+    var element = document.getElementById("myDiv");
+    element.classList.add("purple");
+}
+
+function myFunctionGreen() {
+    var element = document.getElementById("myDiv");
+    element.classList.add("green");
+}
