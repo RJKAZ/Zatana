e30806ef7f9179982219b0b30159a02331ba552a
Richard Kazanowski <rjkaz@comcast.net>
2021-03-08 01:01:38 -0500

    update

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ I need to start this one from scratch, but the key to doing it will be the javsc
 So, I'm gonna start this one fronm stratch
 
 but I'm gonna limit it firest. I need to start off small and with each click get the text color to change. That would be easier then doing the whole effects. Once I can get the color change working, I should be able to get the effects working. 
+
+Another failure -
+
+I set up an event listener to listen to the clicks from two buttons. Console log works, but only one color or the other works. I had this problem with other attempts. Back to the drawing board.
--- attempt_march_7/script.js
+++ attempt_march_7/script.js
@@ -1,3 +1,11 @@
+const fire = document.getElementById('top-left');
+const water = document.getElementById('top-right');
+const sparkle = document.getElementById('bottom-left');
+const electric = document.getElementById('bottom-right');
+const word = document.getElementById('word')
+
+
+
 function reverseString(str){
     var strArr = str.split("");
     var reverseStrArray = strArr.reverse();
@@ -6,11 +14,20 @@ function reverseString(str){
     console.log(reversedString);
     //document.write(reversedString);
     document.getElementById("word").innerHTML=reversedString;
-    var water = document.getElementById("word");
-    water.classList.add("blue");
+
 }
 
 
+fire.addEventListener('click', () =>{
+    document.getElementById("word");
+    console.log('Fire Magic');
+    word.classList.add('fire');
+    
+})
 
-
+water.addEventListener('click', () =>{
+    document.getElementById("word");
+    console.log('Water Magic');
+    word.classList.add('water')
+})
 
